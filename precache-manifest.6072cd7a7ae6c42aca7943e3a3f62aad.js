778e615271894f6daff8e026943e9f083d251bff
titouancreach <titouan.creach@gmail.com>
2018-10-21 17:24:06 +0200

    Updates

---
+++ precache-manifest.6072cd7a7ae6c42aca7943e3a3f62aad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61ee716e99c572744288",
+    "url": "/hello-bus/static/js/runtime~main.61ee716e.js"
+  },
+  {
+    "revision": "15362e101ca68c3c03c7",
+    "url": "/hello-bus/static/js/main.15362e10.chunk.js"
+  },
+  {
+    "revision": "d0b3af45bb1b7e9005f5",
+    "url": "/hello-bus/static/js/1.d0b3af45.chunk.js"
+  },
+  {
+    "revision": "15362e101ca68c3c03c7",
+    "url": "/hello-bus/static/css/main.e360641d.chunk.css"
+  },
+  {
+    "revision": "d0b3af45bb1b7e9005f5",
+    "url": "/hello-bus/static/css/1.e3abf2ad.chunk.css"
+  },
+  {
+    "revision": "26795bec60f5fd2aea471464ebd6f0e1",
+    "url": "/hello-bus/index.html"
+  }
+];
