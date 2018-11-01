2a2d725325cf2af1389588e78d287510629702cd
titouancreach <titouan.creach@gmail.com>
2018-11-01 19:56:13 +0100

    Move from class component to function component

--- src/App.js
+++ src/App.js
@@ -12,19 +12,17 @@ import {HashRouter, Switch, Route} from 'react-router-dom'
 
 library.add(faBus, faHome, faArrowCircleLeft)
 
-class App extends Component {
-  render() {
-    return (
-      <HashRouter>
-        <div className="flex flex-column h-100 pa3 bg-near-black items-center">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/schedule/:line/:stop" component={Schedule} />
-          </Switch>
-        </div>
-      </HashRouter>
-    )
-  }
+function App() {
+  return (
+    <HashRouter>
+      <div className="flex flex-column h-100 pa3 bg-near-black items-center">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/schedule/:line/:stop" component={Schedule} />
+        </Switch>
+      </div>
+    </HashRouter>
+  )
 }
 
 export default App
